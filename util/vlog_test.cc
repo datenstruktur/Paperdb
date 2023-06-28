528f21f5360bad1fb9b235ddb28d295e3a3911ee
WangTingZheng <2251985371@qq.com>
2023-06-28 22:06:16 +0800

    feat(vlog): test sanitizers
    test sanitizers in ci, seems it losed it function

--- util/vlog_test.cc
+++ util/vlog_test.cc
@@ -101,6 +101,66 @@ class VlogTest : public testing::Test {
   Arena arena;
 };
 
+class VlogTestInFS : public testing::Test {
+ public:
+  VlogTestInFS() : reader_(nullptr), source_(nullptr),sink_(nullptr) {
+    Status s;
+    if(!Env::Default()->FileExists("/tmp/vlogtestinfs")){
+      s = Env::Default()->CreateDir("/tmp/vlogtestinfs");
+      if(!s.ok()){
+        return ;
+      }
+    }
+    s = Env::Default()->NewWritableFile("/tmp/vlogtestinfs/test_file", &sink_);
+    if(!s.ok()){
+      return ;
+    }
+    writer_ = new VlogWriter(sink_, 0);
+  }
+
+  void Add(Slice key, Slice value, std::string* handle) {
+    writer_->Add(key, value, handle);
+  }
+
+  Status FinishAdd() {
+    if (reader_ == nullptr) {
+      sink_->Close();
+      Status s = Env::Default()->NewRandomAccessFile("/tmp/vlogtestinfs/test_file", &source_);
+      if(!s.ok()){
+        return s;
+      }
+      reader_ = new VlogReader(source_);
+    }
+    return Status::OK();
+  }
+
+  void Reader(Slice* value, std::string* handle) {
+    FinishAdd();
+    uint64_t entry_size = 0;
+    Slice handle_slice = Slice(handle->data(), handle->size());
+    VlogReader::GetEntrySize(handle_slice, &entry_size);
+
+    char* buf = arena.Allocate(entry_size);
+    reader_->ReadRecond(Slice(handle->data(), handle->size()), value, buf,
+                        entry_size);
+  }
+
+  ~VlogTestInFS() override{
+    if(!Env::Default()->FileExists("/tmp/vlogtestinfs")) {
+      Env::Default()->RemoveDir("/tmp/vlogtestinfs/test_file");
+    }
+  }
+
+ private:
+  VlogWriter* writer_;
+  VlogReader* reader_;
+
+  RandomAccessFile* source_;
+  WritableFile* sink_;
+
+  Arena arena;
+};
+
 TEST_F(VlogTest, Single) {
   std::string handle;
   Add("key", "value", &handle);
@@ -113,6 +173,36 @@ TEST_F(VlogTest, Single) {
 }
 
 TEST_F(VlogTest, Multi) {
+  std::vector<std::string> handles;
+  int N = 1000;
+  for (int i = 0; i < N; i++) {
+    std::string handle;
+    Add("key" + std::to_string(i), "value" + std::to_string(i), &handle);
+    handles.push_back(handle);
+  }
+
+  FinishAdd();
+
+  for (int i = 0; i < N; i++) {
+    Slice value;
+    std::string handle = handles[i];
+    Reader(&value, &handle);
+    ASSERT_EQ(value.ToString(), "value" + std::to_string(i));
+  }
+}
+
+TEST_F(VlogTestInFS, Single) {
+  std::string handle;
+  Add("key", "value", &handle);
+
+  FinishAdd();
+
+  Slice value;
+  Reader(&value, &handle);
+  ASSERT_EQ(value.ToString(), "value");
+}
+
+TEST_F(VlogTestInFS, Multi) {
   std::vector<std::string> handles;
   int N = 1000;
   for (int i = 0; i < N; i++) {
